1a096c0d274cbb8e1c16a996e2ca4d7bdaa7b397
주상후 <jusanghoo0321@naver.com>
2023-03-12 22:58:42 +0900

    'plus_file_upload'

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
-import react, { useEffect, useState } from 'react'
+import react, { useEffect, useState, useRef } from 'react'
 import { dbService, dbAddDoc, dbCollection, dbQuery, dbOnSnapShot } from '../fbase';
 import Nweet from '../components/Nweet';
 
 function Home(props) {
 
+    const fileInput = useRef();
+
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     const onChange = (e) => {
         const {target:{value}} = e;
@@ -21,7 +24,20 @@ function Home(props) {
         });
         setNweet("");
     }
-    
+    const onFileChange = (e) => {
+        const {target : {files}} = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishiedEvent) => {
+            const {currentTarget: {result}} = finishiedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => {
+        setAttachment(null);
+        fileInput.current.value = null;
+    }
 
     useEffect(() => {
         const q = dbQuery(dbCollection(dbService, "nweets"));
@@ -42,12 +58,23 @@ function Home(props) {
                 value={nweet}
                 onChange={onChange}
                 placeholder="What's on your mind?"
-                max={120}
-                />
+                max={120}/>
+                <input 
+                type="file"
+                accept='image/*'
+                onChange={onFileChange}
+                ref={fileInput}/>
+                {
+                    console.log(fileInput.current)
+                }
                 <input
                 type="submit"
-                value="Nweet"
-                />
+                value="Nweet"/>
+                {attachment && 
+                <div>
+                    <img src={attachment} width="50px" height="50px"/>
+                    <button onClick={onClearAttachment}>cancel upload</button>
+                </div>}
             </form>
             {
                 nweets.map((nweet) => {
