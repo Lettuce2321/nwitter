b4dfe0db7800a124258e81e8266715f05d45f3dc
주상후 <jusanghoo0321@naver.com>
2023-03-13 21:41:18 +0900

    'firebase'

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
-import {getFirestore, addDoc, getDocs, collection, query, onSnapshot, deleteDoc, updateDoc, doc } from "firebase/firestore"
+import { getFirestore, addDoc, getDocs, collection, query, onSnapshot, deleteDoc, updateDoc, doc, where, orderBy } from "firebase/firestore"
+import { getStorage, ref, uploadString, getDownloadURL, deleteObject } from "firebase/storage"
 
 const firebaseConfig = {
     apiKey: "AIzaSyANoRFBdNtC5CKwA5zhebUwj-nMyl94At0",
@@ -15,9 +16,17 @@ const app = initializeApp(firebaseConfig);
 export const authService = getAuth();
 export const dbService = getFirestore();
 export const dbAddDoc = addDoc;
+export const dbGetDocs = getDocs;
 export const dbCollection = collection;
 export const dbQuery = query;
 export const dbOnSnapShot = onSnapshot;
 export const dbDoc = doc;
 export const dbdeleteDoc = deleteDoc;
 export const dbUpdataDoc = updateDoc;
+export const dbWhere = where;
+export const dbOrderBy = orderBy;
+export const storageService = getStorage();
+export const stRef = ref;
+export const stUploadString = uploadString;
+export const stGetDownloadURL = getDownloadURL;
+export const stDeleteObject = deleteObject;
