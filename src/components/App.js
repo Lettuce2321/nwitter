55742d2901ac96029e37218a9b2a7a0a6c3735ee
주상후 <jusanghoo0321@naver.com>
2023-03-13 21:45:06 +0900

    'change_update_userObj'

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,30 @@ function App() {
   const [isLoggedin, setLoggedIn] = useState(false);
   const [userObj, setUserobj] = useState(null);
 
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserobj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  }
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
+        console.log(user.displayName);
+        if(user.displayName === null) {
+          const name = user.email.split("@")[0];
+          user.displayName = name;
+        }
+        console.log(user.displayName)
         setLoggedIn(true);
-        setUserobj(user)
+        setUserobj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(user, {displayName: user.displayName}),
+        });
+        
       } else {
         setLoggedIn(false);
       }
@@ -23,7 +42,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedin={isLoggedin} userObj={userObj}/> : "Initializing"}
+      {init ? <AppRouter isLoggedin={isLoggedin} userObj={userObj} refreshUser={refreshUser}/> : "Initializing"}
 
       <footer>&copy;{new Date().getFullYear()} Nwitter </footer>
     </>
